66a165bbe154554edc45f1394f1249ab5242ec92
johnnyasd12 <johnnnyasd12@gmail.com>
2017-06-30 14:05:14 +0800

    onResume() add startDetector();

--- app/src/main/java/com/yutouch/affectivafacear/MainActivity.java
+++ app/src/main/java/com/yutouch/affectivafacear/MainActivity.java
@@ -1,89 +1,172 @@
 package com.yutouch.affectivafacear;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.affectiva.android.affdex.sdk.Frame;
 import com.affectiva.android.affdex.sdk.detector.CameraDetector;
 import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
 
-public class MainActivity extends AppCompatActivity implements CameraDetector.CameraEventListener{
+import java.util.List;
 
-    LinearLayout mainLayout;
-    SurfaceView camServiceView;// 顯示畫面
-    private void initializeUI(){
-        mainLayout = (LinearLayout)findViewById(R.id.linearLayout1);
-        camServiceView = (SurfaceView)findViewById(R.id.surfaceView1);
+/**
+ * This is a very bare sample app to demonstrate the usage of the CameraDetector object from Affectiva.
+ * It displays statistics on frames per second, percentage of time a face was detected, and the user's smile score.
+ *
+ * The app shows off the maneuverability of the SDK by allowing the user to start and stop the SDK and also hide the camera SurfaceView.
+ *
+ * For use with SDK 2.02
+ */
+public class MainActivity extends Activity implements Detector.ImageListener, CameraDetector.CameraEventListener {
 
-    }
+    final String LOG_TAG = "CameraDetectorDemo";
+
+    Button startSDKButton;
+    Button surfaceViewVisibilityButton;
+    TextView smileTextView;
+    TextView ageTextView;
+    TextView ethnicityTextView;
+    ToggleButton toggleButton;
+
+    SurfaceView cameraPreview;
+
+    boolean isCameraBack = false;
+    boolean isSDKStarted = false;
+
+    RelativeLayout mainLayout;
+
+    CameraDetector detector;
+
+    int previewWidth = 0;
+    int previewHeight = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeUI();
-        // 初始化 detector
-        CameraDetector detector = new CameraDetector(this, // context
-                CameraDetector.CameraType.CAMERA_FRONT, // 前鏡頭
-                camServiceView, // 顯示畫面
-                5, // 偵測臉的最多數量
-                Detector.FaceDetectorMode.SMALL_FACES); // 臉部大小偵測偏好
-        detector.setMaxProcessRate(5);// 每秒處理幾個frames, 最低建議設5
-    }
 
+
+        startSDKButton = (Button) findViewById(R.id.sdk_start_button);
+        startSDKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSDKStarted) {
+                    isSDKStarted = false;
+                    stopDetector();
+                    startSDKButton.setText("Start Camera");
+                } else {
+                    isSDKStarted = true;
+                    startDetector();
+                    startSDKButton.setText("Stop Camera");
+                }
+            }
+        });
+        startSDKButton.setText("Start Camera");
+
+        //We create a custom SurfaceView that resizes itself to match the aspect ratio of the incoming camera frames
+        mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        cameraPreview = new SurfaceView(this) {
+            @Override
+            public void onMeasure(int widthSpec, int heightSpec) {
+                int measureWidth = MeasureSpec.getSize(widthSpec);
+                int measureHeight = MeasureSpec.getSize(heightSpec);
+                int width;
+                int height;
+                if (previewHeight == 0 || previewWidth == 0) {
+                    width = measureWidth;
+                    height = measureHeight;
+                } else {
+                    float viewAspectRatio = (float)measureWidth/measureHeight;
+                    float cameraPreviewAspectRatio = (float) previewWidth/previewHeight;
+
+                    if (cameraPreviewAspectRatio > viewAspectRatio) {
+                        width = measureWidth;
+                        height =(int) (measureWidth / cameraPreviewAspectRatio);
+                    } else {
+                        width = (int) (measureHeight * cameraPreviewAspectRatio);
+                        height = measureHeight;
+                    }
+                }
+                setMeasuredDimension(width,height);
+            }
+        };
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT,RelativeLayout.TRUE);
+        cameraPreview.setLayoutParams(params);
+        mainLayout.addView(cameraPreview,0);
+
+        detector = new CameraDetector(this, CameraDetector.CameraType.CAMERA_FRONT, cameraPreview);
+        detector.setDetectSmile(false);
+        detector.setDetectAge(false);
+        detector.setDetectEthnicity(false);
+        detector.setDetectAllAppearances(true);
+        detector.setImageListener(this);
+        detector.setOnCameraEventListener(this);
+
+        isSDKStarted = true; // 這行必須?
+        //startDetector();
+    }
 
     @Override
-    public void onCameraSizeSelected(int cameraWidth, int cameraHeight, Frame.ROTATE rotation) { // 選定畫面SIZE?
-        int cameraPreviewWidth;
-        int cameraPreviewHeight;
-
-//cameraWidth and cameraHeight report the unrotated dimensions of the camera frames, so switch the width and height if necessary
-
-        if (rotation == Frame.ROTATE.BY_90_CCW || rotation == Frame.ROTATE.BY_90_CW) {
-            cameraPreviewWidth = cameraHeight;
-            cameraPreviewHeight = cameraWidth;
-        } else {
-            cameraPreviewWidth = cameraWidth;
-            cameraPreviewHeight = cameraHeight;
+    protected void onResume() {
+        super.onResume();
+        if (isSDKStarted) {
+            startDetector(); // 會影響一開始?
         }
-
-//retrieve the width and height of the ViewGroup object containing our SurfaceView (in an actual application, we would want to consider the possibility that the mainLayout object may not have been sized yet)
-
-        int layoutWidth = mainLayout.getWidth();
-        int layoutHeight = mainLayout.getHeight();
-
-//compute the aspect Ratio of the ViewGroup object and the cameraPreview
-
-        float layoutAspectRatio = (float)layoutWidth/layoutHeight;
-        float cameraPreviewAspectRatio = (float)cameraWidth/cameraHeight;
-
-        int newWidth;
-        int newHeight;
-
-        if (cameraPreviewAspectRatio > layoutAspectRatio) {
-            newWidth = layoutWidth;
-            newHeight =(int) (layoutWidth / cameraPreviewAspectRatio);
-        } else {
-            newWidth = (int) (layoutHeight * cameraPreviewAspectRatio);
-            newHeight = layoutHeight;
-        }
-
-//size the SurfaceView
-
-        ViewGroup.LayoutParams params = camServiceView.getLayoutParams();
-        params.height = newHeight;
-        params.width = newWidth;
-        camServiceView.setLayoutParams(params);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopDetector();
+    }
 
+    void startDetector() {
+        if (!detector.isRunning()) {
+            detector.start();
+        }
+    }
 
+    void stopDetector() {
+        if (detector.isRunning()) {
+            detector.stop();
+        }
+    }
 
+    void switchCamera(CameraDetector.CameraType type) {
+        detector.setCameraType(type);
+    }
 
+    @Override
+    public void onImageResults(List<Face> list, Frame frame, float v) {
+        if (list == null)
+            return;
+        if (list.size() == 0) {
 
+        } else {
+            Face face = list.get(0);
 
+        }
+    }
 
-
+    @SuppressWarnings("SuspiciousNameCombination")
+    @Override
+    public void onCameraSizeSelected(int width, int height, Frame.ROTATE rotate) {
+        if (rotate == Frame.ROTATE.BY_90_CCW || rotate == Frame.ROTATE.BY_90_CW) {
+            previewWidth = height;
+            previewHeight = width;
+        } else {
+            previewHeight = height;
+            previewWidth = width;
+        }
+        cameraPreview.requestLayout();
+    }
 }
